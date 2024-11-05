4a9a77d854d54daa1f71ad716f0441ee874acadf
OlehKorsun <148396649+OlehKorsun@users.noreply.github.com>
2024-11-05 19:40:07 +0100

    Update Main.java

--- Project_4/Main.java
+++ Project_4/Main.java
@@ -1,19 +1,7 @@
-/**
- *
- *  @author Korsun Oleh S30911
- *
- */
-
 package zad1;
 
 
 import java.util.*;
-
-// Plik Main.java może być dowolnie modyfikowany, 
-// ale punkty uzyskuje się za właściwe dzialanie poszczególnych pokazanych tu metod klasy XList.
-
-// Jeżeli nie oprogramujemy wszystkich metod, to z klasy Main nalezy usunąć te fragmenty,
-// które powodują błędy w kompilacji - w przeciwnym razie nie uzyskamy punktów.
 
 public class Main {
   public static void main(String[] args) {
